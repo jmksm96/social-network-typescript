a9e8ab56759975c69efac052570a352d1c9cedb5
Max <jmksm96@gmail.com>
2020-12-15 22:08:56 +0300

    21. Layout Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Max
+                </div>
+                <div className={s.dialog}>
+                    Mark
+                </div>
+                <div className={s.dialog}>
+                    Slava
+                </div>
+                <div className={s.dialog}>
+                    Dimon
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>React</div>
+                <div className={s.message}>Typescript</div>
+                <div className={s.message}>Javascript</div>
+            </div>
         </div>
     )
 }
