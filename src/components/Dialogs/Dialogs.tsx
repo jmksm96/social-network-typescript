4feed0a39685422d4b2f1245febbcc4c6a1c1a52
Max <jmksm96@gmail.com>
2020-12-18 22:52:44 +0300

    25. Map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -13,6 +13,25 @@ export type MessageType = {
 }
 
 
+let dialogs = [
+    {id: 1, name: 'Max'},
+    {id: 2, name: 'Slava'},
+    {id: 3, name: 'Dimon'},
+    {id: 4, name: 'Danila'},
+    {id: 5, name: 'Edik'},
+    {id: 6, name: 'Mark'}
+]
+
+let messages = [
+    {id: 1, message: 'React'},
+    {id: 2, message: 'Typescript'},
+    {id: 3, message: 'Angular'},
+    {id: 4, message: 'Node.js'},
+    {id: 5, message: 'Javascript'},
+    {id: 6, message: 'Vue.js'},
+
+]
+
 const DialogItem: React.FC<DialogsType> = (props) => {
 
     let path = '/dialogs/' + props.id
@@ -32,49 +51,22 @@ const Message: React.FC<MessageType> = (props) => {
     )
 }
 
-
 const Dialogs = () => {
 
-    let dialogsData = [
-        {id: 1, name: 'Max'},
-        {id: 2, name: 'Slava'},
-        {id: 3, name: 'Dimon'},
-        {id: 4, name: 'Danila'},
-        {id: 5, name: 'Edik'},
-        {id: 6, name: 'Mark'}
-    ]
-
-    let messageData = [
-        {id: 1, message: 'React'},
-        {id: 2, message: 'Typescript'},
-        {id: 3, message: 'Angular'},
-        {id: 4, message: 'Node.js'},
-        {id: 5, message: 'Javascript'},
-        {id: 6, message: 'Vue.js'},
-
-    ]
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = messages.map(m => <Message message={m.message}/>)
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+                { dialogsElements }
             </div>
-
             <div className={s.messages}>
-                <Message message={messageData[0].message}/>
-                <Message message={messageData[1].message}/>
-                <Message message={messageData[2].message}/>
-                <Message message={messageData[3].message}/>
-                <Message message={messageData[4].message}/>
-                <Message message={messageData[5].message}/>
+                { messagesElements }
             </div>
         </div>
     )
+
 }
 
 export default Dialogs
