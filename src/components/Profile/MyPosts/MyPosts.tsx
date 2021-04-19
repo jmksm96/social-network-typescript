4cb191d7f726dd6e44e9cab477c80244271a46a5
Max <jmksm96@gmail.com>
2021-04-19 13:45:57 +0300

    87. React.memo

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,35 +1,17 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import s from './MyPosts.module.css';
 import {PostsType, ProfilePageType} from "../../../Typing/typing";
 import Post from "./Post/Post";
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {maxLengthCreator, required } from '../../../utils/validators/validator';
 import { Input } from '../../../common/forms-controls/FormsControls';
+import {log} from "util";
 
 type MyPostsPropsType = {
     addPost: (newPostText: string) => void
     profilePage: ProfilePageType
     newPostText: string
 }
-
-const MyPosts: React.FC<MyPostsPropsType> = (props) => {
-    const myPostsRender = (p: PostsType) => <Post message={p.message} likesCount={p.likesCount} key={p.id}/>
-    let addNewPost = (values: any) => {
-        props.addPost(values.newPostText)
-    }
-
-    return (
-        <div className={s.postsBlock}>
-            <h3> My posts</h3>
-            <MyPostsReduxForm onSubmit={addNewPost}/>
-            <div className={s.posts}>
-                {props.profilePage.posts.map(myPostsRender)}
-            </div>
-        </div>
-    )
-}
-
-    const maxLength10 = maxLengthCreator(10)
 
 const MyPostsForm: React.FC<InjectedFormProps> = (props) => {
     return (
@@ -46,6 +28,28 @@ const MyPostsForm: React.FC<InjectedFormProps> = (props) => {
         </form>
     )
 }
+
+const MyPosts: React.FC<MyPostsPropsType> = React.memo((props: MyPostsPropsType) => {
+    console.log('Render')
+    const myPostsRender = (p: PostsType) =><Post message={p.message} likesCount={p.likesCount} key={p.id}/>
+
+    let addNewPost = (values: any) => {
+        props.addPost(values.newPostText)
+    }
+
+    return (
+        <div className={s.postsBlock}>
+            <h3> My posts</h3>
+            <MyPostsReduxForm onSubmit={addNewPost}/>
+            <div className={s.posts}>
+                {props.profilePage.posts.map(myPostsRender)}
+            </div>
+        </div>
+    )
+})
+
+    const maxLength10 = maxLengthCreator(10)
+
 
 const MyPostsReduxForm = reduxForm({form: 'myPostsAddPostForm'})(MyPostsForm)
 
